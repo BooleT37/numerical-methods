68a84ebe24c76054d7b315b81a5dd5d851d9fb30
Алексей Левин <BooleT37@gmail.com>
2018-01-26 22:02:53 +0500

    Crank-Nicolson method

---
+++ CrankNicolsonMethod.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+from GridMethod import GridMethod
+from TDMAsolver import tdma_solver
+from functionutils import count_g
+
+
+class CrankNicolsonMethod(GridMethod):
+    def __init__(self, n, m, x_max, t_max):
+        super().__init__(n, m, x_max, t_max)
+        self.coeff = self.tau / (self.h ** 2)
+        self.a = self.c = -self.coeff
+        self.b = 1 + (2 * self.tau) / (self.h ** 2)
+        self.aa = np.full(self.n - 2, self.a)
+        self.bb = np.full(self.n - 1, self.b)
+        self.cc = np.full(self.n - 2, self.c)
+
+    def count(self):
+        matrix = self.get_initial_matrix()
+
+        for j in range(1, self.m + 1):
+            xx = tdma_solver(self.aa, self.bb, self.cc, self.count_dd(matrix, j))
+            matrix[j, 1:self.n] = xx
+        return matrix
+
+    def count_dd(self, matrix, j):
+        dd = np.ndarray(shape=self.n - 1)
+        dd[0] = self.count_d(matrix, 1, j) - self.a * matrix[j, 0]
+        for ind in range(1, self.n - 2):
+            dd[ind] = self.count_d(matrix, ind + 1, j)
+        dd[self.n - 2] = self.count_d(matrix, self.n - 1, j) - self.c * matrix[j, self.n]
+        return dd
+
+    def count_d(self, u, i, j):
+        return self.coeff * (u[j - 1, i - 1] - 2 * u[j - 1, i] + u[j - 1, i + 1]) + \
+               self.tau * count_g(i, j - 1, self.h, self.tau) + \
+               u[j - 1, i]
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import math
 from enum import Enum
 
+from CrankNicolsonMethod import CrankNicolsonMethod
 from ExplicitFiniteDifferenceMethod import ExplicitFiniteDifferenceMethod
 from ImplicitFiniteDifferenceMethod import ImplicitFiniteDifferenceMethod
 from functionutils import count_error
@@ -15,62 +16,67 @@ t_max = 10
 class Method(Enum):
     ExplicitFiniteDifference = 1
     ImplicitFiniteDifference = 2
+    CrankNicolson = 3
 
 
 METHOD_TITLES = {
     Method.ExplicitFiniteDifference: "Explicit Finite Difference method",
-    Method.ImplicitFiniteDifference: "Implicit Finite Difference method"
+    Method.ImplicitFiniteDifference: "Implicit Finite Difference method",
+    Method.CrankNicolson: "Crank-Nicolson method"
 }
 
 
 def run():
-    # count_for_default_intervals(Method.ImplicitFiniteDifference)
-    # count_for_different_n(Method.ImplicitFiniteDifference)
-    count_for_different_m(Method.ImplicitFiniteDifference)
+    # count_for_default_intervals(Method.CrankNicolson)
+    count_for_different_n(Method.ImplicitFiniteDifference, 40, 1)
+    count_for_different_m(Method.ImplicitFiniteDifference, 450, 10)
 
 
 def count_for_default_intervals(method):
     if method == Method.ExplicitFiniteDifference:
-        efd = ExplicitFiniteDifferenceMethod(n_start, m_start, x_max, t_max)
-        approx_values = efd.count()
+        method_impl = ExplicitFiniteDifferenceMethod(n_start, m_start, x_max, t_max)
+    elif method == Method.ImplicitFiniteDifference:
+        method_impl = ImplicitFiniteDifferenceMethod(n_start, m_start, x_max, t_max)
     else:
-        ifd = ImplicitFiniteDifferenceMethod(n_start, m_start, x_max, t_max)
-        approx_values = ifd.count()
+        method_impl = CrankNicolsonMethod(n_start, m_start, x_max, t_max)
+    approx_values = method_impl.count()
     error = count_error(approx_values, x_max, t_max)
     show_plot(approx_values, x_max, t_max, error)
 
 
-def count_for_different_n(method):
-    n_end = 40
-    n_step = 1
+def count_for_different_n(method, n_end, n_step):
     errors = []
     nn = range(n_start, n_end, n_step)
     for n in nn:
         if method == Method.ExplicitFiniteDifference:
             approx_values = ExplicitFiniteDifferenceMethod(n, m_start, x_max, t_max).count()
-        else:
+        elif method == Method.ImplicitFiniteDifference:
             approx_values = ImplicitFiniteDifferenceMethod(n, m_start, x_max, t_max).count()
+        else:
+            approx_values = CrankNicolsonMethod(n, m_start, x_max, t_max).count()
         error = count_error(approx_values, x_max, t_max)
         errors.append(error)
     title = METHOD_TITLES[method]
     title += f"\nN = {{{n_start}, {n_start + n_step}, ..., {n_end - n_step * 2}, {n_end - n_step}}}, M = {m_start}"
+    title += f"\nerror(n = {n_end - n_step}) = {errors[len(errors) - 1]}"
     show_errors_plot(errors, nn, title=title, xlabel="N")
 
 
-def count_for_different_m(method):
-    m_end = 450
-    m_step = 10
+def count_for_different_m(method, m_end, m_step):
     errors = []
     mm = range(m_start, m_end, m_step)
     for m in mm:
         if method == Method.ExplicitFiniteDifference:
             approx_values = ExplicitFiniteDifferenceMethod(n_start, m, x_max, t_max).count()
-        else:
+        elif method == Method.ImplicitFiniteDifference:
             approx_values = ImplicitFiniteDifferenceMethod(n_start, m, x_max, t_max).count()
+        else:
+            approx_values = CrankNicolsonMethod(n_start, m, x_max, t_max).count()
         error = count_error(approx_values, x_max, t_max)
         errors.append(error)
     title = METHOD_TITLES[method]
     title += f"\nN = {n_start}, M = {{{m_start}, {m_start + m_step}, ..., {m_end - m_step * 2}, {m_end - m_step}}}"
+    title += f"\nerror(m = {m_end - m_step}) = {errors[len(errors) - 1]}"
     show_errors_plot(errors, mm, title=title, xlabel="M")
 
 
